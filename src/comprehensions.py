3a50d3be1c3671767a5fb0875d775ff3a88009c7
Sheila <s.hopper01110@gmail.com>
2019-04-30 18:06:06 -0400

    comprhensions.py successful

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -15,28 +15,31 @@ for x in range(6):
     y.append(x)
 print (y[1:])
 
-# # Write a list comprehension to produce the cubes of the numbers 0-9:
-# # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+# Write a list comprehension to produce the cubes of the numbers 0-9:
+# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-# y = []
+y = []
+for x in range(10):
+    y.append(x**3)
 
-# print(y)
+print(y)
 
-# # Write a list comprehension to produce the uppercase version of all the
-# # elements in array a. Hint: "foo".upper() is "FOO".
+# Write a list comprehension to produce the uppercase version of all the
+# elements in array a. Hint: "foo".upper() is "FOO".
 
-# a = ["foo", "bar", "baz"]
+a = ["foo", "bar", "baz"]
 
-# y = []
+y = []
+for z in a:
+    y.append(z.upper())
+print(y)
 
-# print(y)
+# Use a list comprehension to create a list containing only the _even_ elements
+# the user entered into list x.
 
-# # Use a list comprehension to create a list containing only the _even_ elements
-# # the user entered into list x.
+x = input("Enter comma-separated numbers: ").split(',')
 
-# x = input("Enter comma-separated numbers: ").split(',')
+# What do you need between the square brackets to make it work?
+y = []
 
-# # What do you need between the square brackets to make it work?
-# y = []
-
-# print(y)
+print(y)
--- src/dictionaries.py
+++ src/dictionaries.py
@@ -35,6 +35,8 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 57, "lon": -250, "name": "location X"})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
