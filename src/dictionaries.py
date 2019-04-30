62aa24405abe0445f197941c87493d9d5905b9d9
Sheila <s.hopper01110@gmail.com>
2019-04-30 18:54:25 -0400

    dictionries.py successful

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -41,6 +41,17 @@ print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+for x in waypoints:
+    if x['name'] == 'a place':
+        x['name'] = 'not a real place'
+        x['lon'] = -130
+
+print(waypoints)
+
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for value in waypoints:
+    for x in list(value):
+        print(value[x])
